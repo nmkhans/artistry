1d90c91755c76589f8579a4783fc5a620cec8b83
nmkhans <nurmoin05@gmail.com>
2025-06-27 01:59:57 +0600

    dynamic title added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
+  const location = useLocation();
+  console.log(location);
+
+  useEffect(() => {
+    window.document.title =
+      location.pathname === "/"
+        ? "Artistry - Historical Articraft showcase"
+        : `Artistry - ${location.pathname
+            .slice(1)
+            .split("-")
+            .join(" ")}`;
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
